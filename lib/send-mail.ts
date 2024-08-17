d1c17b41811832088d987223eef7328ba8c88294
Sarthak <sarth.mahajan2000@gmail.com>
2024-08-17 17:41:33 +0530

    fix email issue

--- lib/send-mail.ts
+++ lib/send-mail.ts
@@ -26,7 +26,7 @@ const sendEmail = async (options: EmailOptions): Promise<void> => {
 
     let emailHtml;
     if (template && data) {
-        const templatePath = path.resolve(process.cwd(), 'public', 'mails', template);
+        const templatePath = path.resolve('mails', template);
         console.log("template", templatePath);
 
         try {
