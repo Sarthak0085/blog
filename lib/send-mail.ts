0758efa4deac01594fd0a341ab7cd6c14887fc33
Sarthak <sarth.mahajan2000@gmail.com>
2024-08-20 19:09:12 +0530

    fix ejs render issue

--- lib/send-mail.ts
+++ lib/send-mail.ts
@@ -2,7 +2,7 @@ import nodemailer, { Transporter } from "nodemailer";
 import ejs from "ejs";
 import path from "path";
 import CustomError from "./customError";
-import { domain } from "./domain";
+import fs from "fs";
 
 interface EmailOptions {
     email: string,
@@ -25,11 +25,35 @@ const sendEmail = async (options: EmailOptions): Promise<void> => {
 
     const { email, subject, template, data, html } = options;
 
+    function listFilesAndDirectories(directoryPath: string) {
+        try {
+            // Read the contents of the directory
+            const items = fs.readdirSync(directoryPath);
+
+            items.forEach(item => {
+                // Create the full path of the item
+                const fullPath = path.join(directoryPath, item);
+
+                // Check if the item is a directory or file
+                const stats = fs.statSync(fullPath);
+
+                if (stats.isDirectory()) {
+                    console.log(`Directory: ${fullPath}`);
+                } else if (stats.isFile()) {
+                    console.log(`File: ${fullPath}`);
+                }
+            });
+        } catch (error) {
+            console.error(`Error reading directory: ${error}`);
+        }
+    }
+
     let emailHtml;
     if (template && data) {
         console.log(process.cwd());
         console.log(process.cwd(), "mails");
-        const templatePath = path.resolve("./public", "mails", template);
+        console.log(listFilesAndDirectories(process.cwd()));
+        const templatePath = path.resolve(process.cwd(), "mails", template);
         console.log("template", templatePath);
 
         try {
